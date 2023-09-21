a152ddfb76eac557345f99157fd63d03042043cd
dinhtrung1605 <127390593+dinhtrungndt@users.noreply.github.com>
2023-09-21 21:28:16 +0700

    up load

---
+++ pages/auth.tsx
@@ -0,0 +1,16 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;

c2e7f89f7f15d0d933ffdb8cd5e7783ec6c9955b
trung.nguyendinh <nguyendinhtrung.it@gmail.com>
2024-02-23 09:26:22 +0700

    up load end file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,23 @@
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
-import  prismadb from '@/lib/prismadb';
 import { compare } from 'bcrypt';
+
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+import  prismadb from '@/lib/prismadb';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 export default NextAuth({
     providers:[
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || '',
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
         Credentials({
             id:'credentials',
             name:'credentials',
@@ -53,6 +66,7 @@ export default NextAuth({
         signIn:'/auth'
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session:{
         strategy: 'jwt',
     },
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,14 @@
 import { useState, useCallback } from "react";
 import Input from "@/components/Input";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -13,6 +20,35 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
@@ -49,9 +85,48 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : "Register"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+w-10
+h-10
+bg-white
+rounded-full
+flex
+justify-center
+items-center
+cursor-pointer
+hover:opacity-80
+transition
+"
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+w-10
+h-10
+bg-white
+rounded-full
+flex
+justify-center
+items-center
+cursor-pointer
+hover:opacity-80
+transition
+"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
@@ -71,39 +146,3 @@ const Auth = () => {
 };
 
 export default Auth;
-
-// Giải thích css
-// bg-[url('/images/hero.jpg')] : background-image: url('/images/hero.jpg'); là thêm ảnh vào background của div đó
-// bg-no-repeat : background-repeat: no-repeat; không lặp lại ảnh
-// bg-fixed : background-attachment: fixed; ảnh sẽ không bị scroll theo trang web
-// bg-cover : background-size: cover; ảnh sẽ được căn chỉnh vừa với kích thước của div
-// bg-black : background-color: #000; màu nền đen
-// bg-opacity-70 : background-color: rgba(0, 0, 0, 0.7); màu nền đen có độ mờ là 0.7
-// px-12 : padding-left: 3rem; padding-right: 3rem;
-// py-5 : padding-top: 1.25rem; padding-bottom: 1.25rem;
-// h-12 : height: 3rem;
-// px-16 : padding-left: 4rem; padding-right: 4rem;
-// py-16 : padding-top: 4rem; padding-bottom: 4rem;
-// self-center : align-self: center là căn giữa theo chiều dọc
-// mt-2 : margin-top: 0.5rem; là căn giữa theo chiều ngang
-// lg:w-2/5 : @media (min-width: 1024px) { width: 40%; } là chiều rộng của div là 40% trên màn hình lớn hơn 1024px
-// lg:max-w-md : @media (min-width: 1024px) { max-width: 28rem; } là chiều rộng tối đa của div là 28rem trên màn hình lớn hơn 1024px
-// rounded-md : border-radius: 0.375rem; là bo tròn các góc của div
-// w-full : width: 100%;
-// text-4xl : font-size: 2rem; kích thước chữ là 2rem
-// mb-8 : margin-bottom: 2rem;
-// font-semibold : font-weight: 600; độ đậm của chữ là 600
-// flex : display: flex; hiển thị theo kiểu flex
-// flex-col : flex-direction: column; hiển thị theo kiểu flex dọc
-// gap-4 : gap: 1rem; là khoảng cách giữa các phần tử là 1rem
-// text-neutral-500 : color: #9ca3af; màu chữ là #9ca3af
-// mt-12 : margin-top: 3rem;
-// text-white : color: #fff; màu chữ trắng
-// ml-1 : margin-left: 0.25rem;
-// hover:underline : hover:underline; khi rê chuột vào thì gạch chân chữ
-// cursor-pointer : cursor: pointer; khi rê chuột vào thì con trỏ chuột sẽ thành dấu mũi tên
-// bg-red-600 : background-color: #dc2626; màu nền đỏ
-// py-3 : padding-top: 0.75rem; padding-bottom: 0.75rem;
-// hover:bg-red-700 : hover:bg-red-700; khi rê chuột vào thì màu nền đỏ sẽ đậm hơn
-// transition : transition: all 150ms ease-in-out; là hiệu ứng chuyển động
-// mt-10 : margin-top: 2.5rem;
